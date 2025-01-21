a93229171c515d03c3815c4a613f945ed2943bd8
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-21 20:47:08 +0530

    conditions

--- catalogue.sh
+++ catalogue.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Installing Nodejs"
 useradd roboshop
 VALIDATE $? "Creating Roboshop user" 
 
-#mkdir /app
+mkdir /app
 VALIDATE $? "Creating App Directory" 
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
