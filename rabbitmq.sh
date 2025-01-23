454889ac53506d89c6b206d31c8befbe01c662d0
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-23 12:03:10 +0530

    conditions

---
+++ rabbitmq.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ 
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>LOGFILE
+
+echo "script started    exceduing at $TIMESTAMP" &>> $LOGFILE
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2...$G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root access"
+    exit 1 #you can give other than 0
+else
+    echo "you are in root user"
+fi #fi means reverse of if, including condtion end.
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+dnf install rabbitmq-server -y 
+VALIIDATE $? "Installing rabbitmq"
+
+systemctl enable rabbitmq-server 
+VALIDATE $? "Enableing rabbitmq"
+
+rabbitmqctl add_user roboshop roboshop123
+VALIDATE $? "Adining roboshop user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VALIDATE $? "permissions"
